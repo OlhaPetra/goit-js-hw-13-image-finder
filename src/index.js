5276c43e88ab02bc9cf5e96f348ab4c975cb573b
OlhaPetra <80599236+OlhaPetra@users.noreply.github.com>
2021-09-25 20:45:20 +0300

    add templ

--- src/index.js
+++ src/index.js
@@ -1,37 +1,36 @@
-import NewApiService from './js/apiService'
+import ImagesApiService from './js/apiService.js';
+import imgTempl from './tamplate/imgTempl.hbs';
 
 const ref = {
   searchForm: document.getElementById('search-form'),
   imgContainer: document.querySelector('.images'),
-  loadMoreBtn: document.querySelector('[data-action="load-more"]')
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'),
 };
 
-/* 
-console.log(ref.imgContainer) 
-console.log(ref.loadMoreBtn)*/
-
 ref.searchForm.addEventListener('submit', onSearch);
-ref.loadMoreBtn.addEventListener('click', OnLoadMore)
+ref.loadMoreBtn.addEventListener('click', OnLoadMore);
 
-let searchQuery = ''
+const imagesApiService = new ImagesApiService();
 
 function onSearch(e) {
   e.preventDefault();
 
-
-  searchQuery = e.currentTarget.elements.query.value;
-  
+  imagesApiService.query = e.currentTarget.elements.query.value.trim();
+  imagesApiService.resetPage();
+  imagesApiService.fetchImages().then(imgs => {
+    clearImgMarkup();
+    appendImgMarkup(imgs);
+  });
 }
 
-
 function OnLoadMore() {
-/*   const API_KEY = '23556027-7518a6338651e19ee58531f7f';
-  const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
-    const searchQuery = e.currentTarget.elements.query.value;
-    console.log(searchQuery)
+  imagesApiService.fetchImages().then(appendImgMarkup);
+}
 
-  fetch(`${BASE_URL}&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`)
-    .then(r => r.json())
-    .then(console.log); 
-*/
+function appendImgMarkup(imgs) {
+  ref.imgContainer.insertAdjacentHTML('beforeend', imgTempl(imgs));
+}
+
+function clearImgMarkup() {
+  ref.imgContainer.innerHTML = '';
 }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,12 +1,37 @@
-export default class NewApiService {
-  constructor() {}
+export default class ImagesApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 12;
+  }
 
   fetchImages() {
     const API_KEY = '23556027-7518a6338651e19ee58531f7f';
     const BASE_URL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
 
-    fetch(`${BASE_URL}&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`)
+    return fetch(
+      `${BASE_URL}&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&key=${API_KEY}`,
+    )
       .then(r => r.json())
-      .then(console.log);
+      .then(data => {
+        this.incrementPage();
+        return data.hits
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
   }
 }
